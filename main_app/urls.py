6634de7a5241c9baeabf2ec96f42398428f55606
joudhyasat <joudhyasat04@gmail.com>
2025-10-11 21:57:18 +0300

    login-page is done

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     # Routes will be added here
      path('', views.home, name='home'),
+     path('login/', views.login, name='login')
+    #  path('accounts/signup/', views.signup, name='signup'),
 ]
