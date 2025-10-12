4c719bce8340d5407e71173afca7f493be849522
DanyahOdeh <danyah.odeh11@gmail.com>
2025-10-12 21:11:31 +0300

    working on the destinations model UI

--- main_app/views.py
+++ main_app/views.py
@@ -71,21 +71,39 @@ def signup(request):
 def destination_list(request):
     destinations = Destination.objects.filter(status='approved')
     return render(request, 'destinations/list.html',{'destinations':destinations})
+
 def destination_detail(request, pk):
     destination = get_object_or_404(Destination, pk=pk)
     destination = get_object_or_404(Destination, pk=pk)
     return render(request, 'destinations/detail.html',{'destination': destination})
+
 @login_required
 def add_destination(request):
     if request.method == 'POST':
         form = DestinationForm(request.POST, request.FILES)
         if form.is_valid():
             destination = form.save(commit = False)
-            destination.created_by = request.user 
+            #destination.created_by = request.user
             destination.save()
             return redirect('destination_list')
-        else:
+    else:
             form = DestinationForm()
+    return render(request, 'destinations/add.html',{'form': form})
 
-        return render(request, 'destinations/add.html',{'form': form})
-        
+def destination_list(request):
+    query = request.GET.get('q', '')
+    category = request.GET.get('category', '')
+
+    destinations = Destination.objects.filter(status='approved')
+
+    if query:
+        destinations = destinations.filter(name__icontains=query)
+
+    if category and category != 'all':
+        destinations = destinations.filter(category=category)
+
+    return render(request, 'destinations/list.html', {
+        'destinations': destinations,
+        'query': query,
+        'selected_category': category,
+    })
